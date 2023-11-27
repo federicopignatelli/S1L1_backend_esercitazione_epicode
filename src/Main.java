e26c13c810b8180c53e97e8b7dd520d8b791e65e
federicopignatelli <federicopignatelli@icloud.com>
2023-11-27 16:23:10 +0100

    esercizi

--- src/Main.java
+++ src/Main.java
@@ -18,4 +18,5 @@ public class Main {
         Integer.toString(b);
         return a + b;
     }
+
 }
--- src/Secondmain.java
+++ src/Secondmain.java
@@ -1,5 +1,18 @@
+import java.util.Scanner;
+
 public class Secondmain {
     public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
 
+        System.out.println("dimmi il tuo nome");
+        String name = scanner.nextLine();
+
+        System.out.println("dimmi dove abiti");
+        String location = scanner.nextLine();
+
+        System.out.println("cosa ti piace mangiare?");
+        String food = scanner.nextLine();
+
+        System.out.println("ciao "+name+" abiti a "+location+" e ti piace la/il "+food);
     }
 }
