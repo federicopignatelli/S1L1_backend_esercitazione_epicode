f778a21db1e7992c59b087b76738c1d67e8236c9
federicopignatelli <federicopignatelli@icloud.com>
2023-11-27 15:00:14 +0100

    ulterior prove per github

--- src/Main.java
+++ src/Main.java
@@ -2,10 +2,6 @@
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        //Que è la mia prima repo
-        System.out.println("lala");
-        System.out.println("lala");
-        System.out.println("lala");
-        System.out.println("lala");
+
         }
     }
---
+++ src/Secondmain.java
@@ -0,0 +1,5 @@
+public class Secondmain {
+    public static void main(String[] args){
+
+    }
+}
